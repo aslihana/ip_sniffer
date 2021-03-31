2eb4779b8d27a9e030f1db7cce77d4d2ae01cd89
Aslihan Akbiyik <aslihana@yandex.com>
2021-03-31 16:08:17 +0300

    first commit

---
+++ target/debug/deps/ip_sniffer.d
@@ -0,0 +1,5 @@
+/Users/aslihanakbiyik/rustProjects/ip_sniffer/target/debug/deps/ip_sniffer: src/main.rs
+
+/Users/aslihanakbiyik/rustProjects/ip_sniffer/target/debug/deps/ip_sniffer.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/ip_sniffer.d
@@ -0,0 +1,1 @@
+/Users/aslihanakbiyik/rustProjects/ip_sniffer/target/debug/ip_sniffer: /Users/aslihanakbiyik/rustProjects/ip_sniffer/src/main.rs
